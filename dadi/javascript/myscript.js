a9f57a15672fbfc9b3bd78da1ac720a5a4274b80
Daniele <danlup94@gmail.com>
2020-10-22 17:42:08 +0200

    Fix js

--- dadi/javascript/myscript.js
+++ dadi/javascript/myscript.js
@@ -1,14 +1,15 @@
 var bottone = document.getElementById('dado')
 
 bottone.addEventListener('click', function() {
-  risultatoai.innerHTML = Math.floor(Math.random() *  6);
-  risultatouser.innerHTML = Matf.floor(Math.random() * 6);
-}
+  risultatoai.innerHTML = Math.floor(Math.random() *  6) + 1 ;
+  risultatouser.innerHTML = Matf.floor(Math.random() * 6) + 1;
 
-if (risultatoai > risultatouser) {
-  document.getElementById('vincitore').innerHTML = "Ho vinto io!"
-} else if (risultatouser > risultatoai) {
-  document.getElementById('vincitore').innerHTML = "Hai vinto tu!"
-} else {
-  document.getElementById('vincitore').innerHTML = "Pari! Tiriamo ancora!"
-}
+  if (risultatoai > risultatouser) {
+    document.getElementById('vincitore').innerHTML = "Ho vinto io!"
+  } else if (risultatouser > risultatoai) {
+    document.getElementById('vincitore').innerHTML = "Hai vinto tu!"
+  } else {
+    document.getElementById('vincitore').innerHTML = "Pari! Tiriamo ancora!"
+  }
+
+} );
