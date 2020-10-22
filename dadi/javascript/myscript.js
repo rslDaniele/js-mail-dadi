a1a16f49ff713bfeed1781bff9994ebde0dbcc85
Daniele <danlup94@gmail.com>
2020-10-22 17:38:56 +0200

    Inserimento regole js

--- dadi/javascript/myscript.js
+++ dadi/javascript/myscript.js
@@ -4,3 +4,11 @@ bottone.addEventListener('click', function() {
   risultatoai.innerHTML = Math.floor(Math.random() *  6);
   risultatouser.innerHTML = Matf.floor(Math.random() * 6);
 }
+
+if (risultatoai > risultatouser) {
+  document.getElementById('vincitore').innerHTML = "Ho vinto io!"
+} else if (risultatouser > risultatoai) {
+  document.getElementById('vincitore').innerHTML = "Hai vinto tu!"
+} else {
+  document.getElementById('vincitore').innerHTML = "Pari! Tiriamo ancora!"
+}
